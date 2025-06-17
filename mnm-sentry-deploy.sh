dd9ebc07f6338010b86a3af7efc8b3866a65a185
Sergei T <shadow.tehb@gmail.com>
2025-06-17 12:57:10 +0300

    DEV-291: Update configs

--- mnm-sentry-deploy.sh
+++ mnm-sentry-deploy.sh
@@ -19,19 +19,40 @@ sudo apt-get update
 sudo apt-get install -y \
   docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+cat << EOF | sudo tee /etc/docker/daemon.json
+{
+  "debug": true,
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "16k",
+    "max-file": "4"
+  }
+}
+EOF
+
+sudo systemctl restart docker
+
 # Создаём пользователя
-sudo useradd -m -s /usr/sbin/nologin sentry
+sudo useradd -m -s /bin/bash sentry
+sudo usermod -aG docker sentry
 
 # Скачиваем репозиторий
-sudo su sentry
-cd /home/sentry
-git clone git@github.com:MathAndMagic/mnm-sentry.git
-
 # ssh пока не используем, потому что нужно добавлять свой ключ на сервер
 # sudo -u sentry git clone git@github.com:MathAndMagic/mnm-sentry.git /home/sentry/mnm-sentry
 # т.к. fork репозитория является публичным, то копируем через https
 sudo -u sentry \
   git clone -b mnm-sentry https://github.com/MathAndMagic/mnm-sentry.git /home/sentry/mnm-sentry
 
-# Запускаем sentry
-sudo docker-compose up -d -f /home/sentry/mnm-sentry/docker-compose.yml
+sudo -u sentry \
+  bash -c 'cd /home/sentry/mnm-sentry && ./install.sh --report-self-hosted-issues'
+
+cat << EOF > /dev/stdout
+Update sentry/config.yaml and .env
+Then run commands
+
+sudo -u sentry \
+  bash -c 'cd /home/sentry/mnm-sentry && ./install.sh --report-self-hosted-issues'
+
+sudo -u sentry \
+  bash -c 'cd /home/sentry/mnm-sentry &&  docker compose up -d'
+EOF
